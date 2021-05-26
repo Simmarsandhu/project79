ba78f1c76c5c3fab3370cb6b1ee3c418338aa2a1
Simmarsandhu <77791732+Simmarsandhu@users.noreply.github.com>
2021-05-26 10:56:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ htmldata="<section class='cards'>"
 for(var i=0; i<menu_list_array.length;i++
     ){
         htmldata=htmldata+'<div class="card">'
-        +'<img src="images/pizzaImg.png">'+menu_list_array[i]+'</div>'
+        +'<img src="pizzaImg.png">'+menu_list_array[i]+'</div>'
     }
     htmldata=htmldata+"</section>"
     document.getElementById("display_addedmenu").innerHTML=htmldata;
